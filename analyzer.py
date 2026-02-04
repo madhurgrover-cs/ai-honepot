34a036f1d525cc843d7778a1db55baec3bb613ed
Madhur Grover <madhurgrover835@gmail.com>
2026-02-04 16:04:08 +0530

    Fix analyzer attack_type constants

--- analyzer.py
+++ analyzer.py
@@ -2,13 +2,16 @@ def analyze_request(payload: str):
     payload = payload.lower()
 
     if "union select" in payload or "' or 1=1" in payload:
-        return "SQL Injection"
+        return "SQL_INJECTION"
+
     if "<script>" in payload:
         return "XSS"
-    if "../" in payload:
-        return "Path Traversal"
-    if "cmd=" in payload or ";ls" in payload:
-        return "Command Injection"
 
-    return "Normal"
+    if "../" in payload:
+        return "PATH_TRAVERSAL"
+
+    if "cmd=" in payload or ";ls" in payload:
+        return "CMD_INJECTION"
+
+    return "NORMAL"
 
